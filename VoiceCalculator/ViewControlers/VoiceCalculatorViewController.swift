88daf76ea05d2294e66f37b8c9d64d479d2745ad
Filip Szukala <filip.szukala@gmail.com>
2017-06-10 13:29:28 +0200

    Changed structure of project.

---
+++ VoiceCalculator/ViewControlers/VoiceCalculatorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCalculatorViewController.swift
+//  VoiceCalculator
+//
+//  Created by Filip Szukala on 10/06/2017.
+//  Copyright © 2017 Filip Szukala. All rights reserved.
+//
+
+import UIKit
+
+class VoiceCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
